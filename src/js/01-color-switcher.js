a339054254f01e55f44e27c4ad7d7f816ceb7621
Danylo Altukhov <dan.97198@gmail.com>
2022-07-22 15:33:12 +0300

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onSwitchColor);
+
+function onSwitchColor() {
+  refs.startBtn.disabled = true;
+  const colorTimeout = setTimeout(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    onSwitchColor();
+  }, 1000);
+  refs.stopBtn.addEventListener('click', onRemoveSwitchColor);
+
+  function onRemoveSwitchColor() {
+    refs.startBtn.disabled = false;
+    clearTimeout(colorTimeout);
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
